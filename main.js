115c7f3760bff693fea3e7ce71da4f144e1c44c4
OEAMO <omadkdklilipo@gmail.com>
2025-11-10 04:34:16 +0200

    Delete main.js
    Signed-off-by: OEAMO <omadkdklilipo@gmail.com>

--- main.js
+++
@@ -1,7 +0,0 @@
-this.addEventListener("load", () => {
-  let intro = document.getElementById("intro");
-  setTimeout(() => {
-    intro.classList.add('hide')
-    document.body.style.overflow = "auto"
-  }, 2000);
-});
